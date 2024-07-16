a0bdfd80dca691c27060bd48ebb801199c810301
HassanBoukhamseen <60622951+HassanBoukhamseen@users.noreply.github.com>
2024-07-16 14:20:16 +0300

    Update base.py

--- app/database/Schemas/base.py
+++ app/database/Schemas/base.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.declarative import declarative_base
-from book_ims.database.connector import connect_to_db
+from app.database.connector import connect_to_db
 
 engine, session = connect_to_db()
 Base = declarative_base()
