7772d06d75871eb176b923ce6a69be76734841bb
HassanBoukhamseen <60622951+HassanBoukhamseen@users.noreply.github.com>
2024-07-16 14:09:55 +0300

    Delete schemas directory

--- schemas/author.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-class Author(BaseModel):
-    author_id: int
-    name: str
-    biography: str
--- schemas/user.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-class User(BaseModel):
-    fname: str
-    lname: str
-    email: str
-    hashedpw: str
-    role: str
