f7834b7c0f26a0750c10c5379df0e7ab195d4f22
HassanBoukhamseen <60622951+HassanBoukhamseen@users.noreply.github.com>
2024-07-16 14:09:38 +0300

    Delete database directory

--- database/Schemas/author.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import Column, String, Integer, Text
-from book_ims.database.Schemas.base import Base
-
-class Author(Base):
-    __tablename__ = 'authors'
-    author_id = Column('author_id', Integer, primary_key=True)
-    name = Column('name', String(100))
-    biography = Column('biography', Text)
