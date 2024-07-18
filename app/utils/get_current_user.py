d2b532653144aaf202243280f6c214ab40584388
HassanBoukhamseen <60622951+HassanBoukhamseen@users.noreply.github.com>
2024-07-18 10:21:43 +0300

    Update get_current_user.py

--- app/utils/get_current_user.py
+++ app/utils/get_current_user.py
@@ -1,9 +1,11 @@
-from fastapi import HTTPException
-from app.services.token import verify_token
+from fastapi import HTTPException, Header
+from typing import Annotated
+from app.services.token_services import verify_token
 
-def get_current_user(token: str):
-    if not token:
+def get_current_user(authorization: Annotated[str, Header()]):
+    if not authorization:
         raise HTTPException(status_code=403, detail="Not authenticated")
+    token = authorization.split(" ")[1] if " " in authorization else authorization
     user = verify_token(token)
     if not user:
         raise HTTPException(status_code=403, detail="Invalid token")
